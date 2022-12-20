bc801b474c151aab842d2a6f7c969b014d6c4110
thzero <thzero@users.noreply.github.com>
2022-12-20 07:38:36 -0600

    updates

---
+++ openSource.js
@@ -0,0 +1,32 @@
+export default () => {
+    return [
+        {
+            category: 'client',
+            name: '@thzero/library_client',
+            url: 'https://github.com/thzero/library_client',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/thzero/library_client/blob/master/license.md'
+        },
+        {
+            category: 'client',
+            name: '@thzero/library_client_service_rest_axios',
+            url: 'https://github.com/thzero/library_client_service_rest_axios',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/thzero/library_client_service_rest_axios/blob/master/license.md'
+        },
+        {
+            category: 'client',
+            name: '@thzero/library_common',
+            url: 'https://github.com/thzero/library_common',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/thzero/library_common/blob/master/license.md'
+        },
+        {
+            category: 'client',
+            name: 'axios',
+            url: 'https://github.com/axios/axios',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/axios/axios/blob/master/LICENSE'
+        },
+    ];
+}
